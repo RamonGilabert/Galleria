c87a2b3f09c4724f41152d774998a9fd25e5692b
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-12-31 14:12:36 +0100

    Adds some js

--- public/scripts.js
+++ public/scripts.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   var dotNavigation = document.getElementsByClassName("dot-navigation")[0];
+  var links = dotNavigation.getElementsByTagName("a");
 
-  dotNavigation.addEventListener("click", function() {
-    var links = dotNavigation.getElementsByTagName("a");
-
-    for (i = 0; i < links.length; i++) {
-      links[i].classList.toggle("current");
-    }
-  });
+  for (i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+      this.classList.toggle("current");
+    });
+  }
 })
