82f2d500245883614776643c8c0fc7ceb799bfa4
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-12-31 13:58:46 +0100

    Adds some styling

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log(document.activeElement)
+  var dotNavigation = document.getElementsByClassName("dot-navigation")[0];
+
+  dotNavigation.addEventListener("click", function() {
+    var links = dotNavigation.getElementsByTagName("a");
+
+    for (i = 0; i < links.length; i++) {
+      links[i].classList.toggle("current");
+    }
+  });
 })
