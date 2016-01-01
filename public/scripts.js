f0a3903c871825b171851f4e6fdb7d386aecf4d6
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-01 19:04:56 +0100

    Adds most of the animation

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
   var dotNavigation = document.getElementsByClassName("dot-navigation")[0];
-  var links = dotNavigation.getElementsByTagName("a");
+  var firstLink = dotNavigation.getElementsByTagName("a")[0];
+  var secondLink = dotNavigation.getElementsByTagName("a")[1];
+  var thirdLink = dotNavigation.getElementsByTagName("a")[2];
+  var galleryContainer = document.getElementsByClassName("galleria-container")[0];
 
-  for (i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function() {
-      this.classList.toggle("current");
-    });
+  function linkClicked() {
+    firstLink.classList.remove("current");
+    secondLink.classList.remove("current");
+    thirdLink.classList.remove("current");
+    this.classList.add("current");
+
+    if (this === firstLink) {
+      galleryContainer.style.transform = "translateX(0%)";
+    } else if (this === secondLink) {
+      galleryContainer.style.transform = "translateX(-100%)";
+    } else {
+      galleryContainer.style.transform = "translateX(-200%)";
+    }
   }
+
+  firstLink.addEventListener("click", linkClicked);
+  secondLink.addEventListener("click", linkClicked);
+  thirdLink.addEventListener("click", linkClicked);
+  firstLink.classList.add("current");
 })
