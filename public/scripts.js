bc9c79984e4ec6bbcc26e630b55ede038d03ce83
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-01 19:21:02 +0100

    Making it work

--- public/scripts.js
+++ public/scripts.js
@@ -1,27 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
   var dotNavigation = document.getElementsByClassName("dot-navigation")[0];
-  var firstLink = dotNavigation.getElementsByTagName("a")[0];
-  var secondLink = dotNavigation.getElementsByTagName("a")[1];
-  var thirdLink = dotNavigation.getElementsByTagName("a")[2];
+  var links = dotNavigation.getElementsByTagName("a");
+
   var galleryContainer = document.getElementsByClassName("galleria-container")[0];
 
-  function linkClicked() {
-    firstLink.classList.remove("current");
-    secondLink.classList.remove("current");
-    thirdLink.classList.remove("current");
-    this.classList.add("current");
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+      for (var i = 0; i < links.length; i++) { links[i].classList.remove("current"); }
 
-    if (this === firstLink) {
-      galleryContainer.style.transform = "translateX(0%)";
-    } else if (this === secondLink) {
-      galleryContainer.style.transform = "translateX(-100%)";
-    } else {
-      galleryContainer.style.transform = "translateX(-200%)";
-    }
+      this.classList.add("current");
+
+      var translation = -100 * value;
+
+      if (this === links[0]) {
+        galleryContainer.style.transform = "translateX(" + translation + "%)";
+      } else if (this === links[1]) {
+        galleryContainer.style.transform = "translateX(-100%)";
+      } else {
+        galleryContainer.style.transform = "translateX(-200%)";
+      }
+    });
   }
 
-  firstLink.addEventListener("click", linkClicked);
-  secondLink.addEventListener("click", linkClicked);
-  thirdLink.addEventListener("click", linkClicked);
-  firstLink.classList.add("current");
+  links[0].classList.add("current");
 })
