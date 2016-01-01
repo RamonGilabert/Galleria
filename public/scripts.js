8ac8012eeb0558ad9c9fa9b9750d531a182cb4d6
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-01 19:31:48 +0100

    Refactored the JS code

--- public/scripts.js
+++ public/scripts.js
@@ -1,26 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
   var dotNavigation = document.getElementsByClassName("dot-navigation")[0];
-  var links = dotNavigation.getElementsByTagName("a");
+  var links = Array.prototype.slice.call(dotNavigation.getElementsByTagName("a"));
 
   var galleryContainer = document.getElementsByClassName("galleria-container")[0];
 
-  for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function() {
-      for (var i = 0; i < links.length; i++) { links[i].classList.remove("current"); }
+  links.forEach(function(element) {
+    element.addEventListener("click", function() {
+      links.forEach(function(element) { element.classList.remove("current"); })
 
       this.classList.add("current");
+      var value = links.indexOf(this);
 
       var translation = -100 * value;
-
-      if (this === links[0]) {
-        galleryContainer.style.transform = "translateX(" + translation + "%)";
-      } else if (this === links[1]) {
-        galleryContainer.style.transform = "translateX(-100%)";
-      } else {
-        galleryContainer.style.transform = "translateX(-200%)";
-      }
+      galleryContainer.style.transform = "translateX(" + translation + "%)";
     });
-  }
+  })
 
   links[0].classList.add("current");
 })
